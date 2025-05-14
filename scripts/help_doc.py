af9884ad5935d4ab1620530bb6e30053a97e8d58
EneaDim <eneadim95@gmail.com>
2025-05-14 16:44:33 +0200

    Help Documentation update

--- scripts/help_doc.py
+++ scripts/help_doc.py
@@ -7,11 +7,10 @@ msg='\033[38;5;214m\n\
 \n   .Run "make hjson" to create the .hjson template which \
 \n    will be used to generate both doc and regmap\n\
 \n - DOCUMENTATION\
-\n   .Run "make md" to generate the markdown documentation file\
+\n   .Run "make doc" to generate the markdown documentation file\
 \n    from the hjson file created before\n\
 \n - REGMAP\
-\n   .Run "make reg" to generate the regmap file for integration\
-\n    with opentitan system\
-\n    Compilation of regmap still not supported\n\
+\n   .Run "make reg" to generate the regmap file for SoC integration\
+\n    with TLUL interconnect based system\
 \033[0;0m'
 print(msg)
