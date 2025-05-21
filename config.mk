36f3928a576bf2c3f800f5a37c36c8c00d7431d6
EneaDim <eneadim95@gmail.com>
2025-05-21 22:05:12 +0200

    Include rtl directory added in LINT_FLAGS

--- config.mk
+++ config.mk
@@ -37,7 +37,7 @@ LINT_FLAGS      := --lint-only -Wall -Wno-fatal --timing +incdir+$(RTLDIR) \
 # COMPILE FLAG
 IVERILOG_FLAGS  := -g2012 -v -Iips/pkgs -I$(RTLDIR) -I$(TBDIR)
 VERILATOR_FLAGS := -Wall -Wno-fatal --binary --timing --Mdir $(SIMDIR)/$(COMPILER) \
-                   +incdir+$(TBDIR) +incdir+model \
+                   +incdir+$(RTLDIR) +incdir+$(TBDIR) +incdir+model \
 									 +incdir+ips/pkgs +incdir+ips/prim_opentitan +incdir+ips/tlul
 # SIMULATION
 TESTBENCH       ?= $(TOP)_tb
