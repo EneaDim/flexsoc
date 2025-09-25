ff7eb1662333af6c28e68d2f4ee392760b2ed76c
EneaDim <eneadim95@gmail.com>
2025-09-25 11:27:44 +0200

    Trial

--- deps.sh
+++ deps.sh
@@ -11,7 +11,7 @@ install_ip_dependencies() {
     sudo apt upgrade
     sudo apt-get install -y --no-install-recommends build-essential clang bison flex 
       nodejs npm libreadline-dev gawk tcl-dev libffi-dev git autoconf cmake libelf-dev \
-    	curl graphviz xdot pkg-config libboost-system-dev help2man swig libeigen3-dev \
+    	curl graphviz xdot pkg-config libboost-system-dev help2man autoconf swig libeigen3-dev \
     	libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
     echo "|**********************************************************************************|"
     echo "|                               Installing iverilog                                |"
