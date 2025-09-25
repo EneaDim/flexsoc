30d8c6db90bfc4a6333975ad93cdfccc0242ae0a
EneaDim <eneadim95@gmail.com>
2025-09-25 21:16:40 +0200

    Try

--- deps.sh
+++ deps.sh
@@ -9,64 +9,64 @@ install_ip_dependencies() {
     cd
     sudo apt update
     sudo apt upgrade
-    sudo apt-get install -y --no-install-recommends build-essential clang bison flex \
-      nodejs npm libreadline-dev gawk tcl-dev libffi-dev git autoconf cmake libelf-dev \
-    	curl graphviz xdot pkg-config libboost-system-dev help2man autoconf swig libeigen3-dev \
-    	libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
-    echo "|**********************************************************************************|"
-    echo "|                               Installing iverilog                                |"
-    echo "|**********************************************************************************|"
-    echo
-    sudo apt-get install -y iverilog --assume-yes
-    echo "|**********************************************************************************|"
-    echo "|                                Installing gtkwave                                |"
-    echo "|**********************************************************************************|"
-    echo
-    cd
-    sudo apt install gtkwave --assume-yes
-    echo
-    echo
-    echo "|**********************************************************************************|"
-    echo "|                                Installing sv2v                                   |"
-    echo "|**********************************************************************************|"
-    echo
-    cd
-    curl -sSL https://get.haskellstack.org/ | sh
-    git clone https://github.com/zachjs/sv2v.git
-    cd sv2v
-    stack setup
-    stack install
-    echo
-    sudo cp $HOME/.local/bin/sv2v /usr/local/bin
-    echo
-    echo
-    echo "|**********************************************************************************|"
-    echo "|                                Installing verilator                              |"
-    echo "|**********************************************************************************|"
-    echo
-    cd
-    git clone https://github.com/verilator/verilator
-    cd verilator
-    git checkout v5.036 
-    autoconf
-    ./configure
-    make -j 2
-    sudo make install
-    sudo cp bin/* /usr/local/bin
-    echo
-    echo "|**********************************************************************************|"
-    echo "|            Cloning yosys and then will start the installation of yosys           |"
-    echo "|**********************************************************************************|"
-    echo
-    cd
-    git clone https://github.com/YosysHQ/yosys.git
-    cd yosys
-    git submodule update --init
-    make -j 2
-    sudo make install
-    sudo cp bin/* /usr/local/bin
-    cd
-    echo
+    sudo apt-get install -y build-essential clang bison flex nodejs npm \
+      libreadline-dev gawk tcl-dev libffi-dev git autoconf cmake libelf-dev \
+    	curl graphviz xdot pkg-config libboost-system-dev help2man autoconf swig \
+      libeigen3-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
+    #echo "|**********************************************************************************|"
+    #echo "|                               Installing iverilog                                |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #sudo apt-get install -y iverilog --assume-yes
+    #echo "|**********************************************************************************|"
+    #echo "|                                Installing gtkwave                                |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #cd
+    #sudo apt install gtkwave --assume-yes
+    #echo
+    #echo
+    #echo "|**********************************************************************************|"
+    #echo "|                                Installing sv2v                                   |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #cd
+    #curl -sSL https://get.haskellstack.org/ | sh
+    #git clone https://github.com/zachjs/sv2v.git
+    #cd sv2v
+    #stack setup
+    #stack install
+    #echo
+    #sudo cp $HOME/.local/bin/sv2v /usr/local/bin
+    #echo
+    #echo
+    #echo "|**********************************************************************************|"
+    #echo "|                                Installing verilator                              |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #cd
+    #git clone https://github.com/verilator/verilator
+    #cd verilator
+    #git checkout v5.036 
+    #autoconf
+    #./configure
+    #make -j 2
+    #sudo make install
+    #sudo cp bin/* /usr/local/bin
+    #echo
+    #echo "|**********************************************************************************|"
+    #echo "|            Cloning yosys and then will start the installation of yosys           |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #cd
+    #git clone https://github.com/YosysHQ/yosys.git
+    #cd yosys
+    #git submodule update --init
+    #make -j 2
+    #sudo make install
+    #sudo cp bin/* /usr/local/bin
+    #cd
+    #echo
     echo "|**********************************************************************************|"
     echo "|                              OpenSTA installation                                |"
     echo "|**********************************************************************************|"
