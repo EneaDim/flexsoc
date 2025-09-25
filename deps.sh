e6dfd4579f952e7076fad5b8435dfc8d6db1b03e
EneaDim <eneadim95@gmail.com>
2025-09-25 08:52:10 +0200

    Nodejs and npn back into deps for netlistsvg

--- deps.sh
+++ deps.sh
@@ -9,7 +9,7 @@ install_ip_dependencies() {
     cd
     sudo apt update
     sudo apt upgrade
-    sudo apt-get install build-essential clang bison flex \
+    sudo apt-get install build-essential clang bison flex nodejs npm\
     	libreadline-dev gawk tcl-dev libffi-dev git autoconf cmake libelf-dev \
     	curl graphviz xdot pkg-config libboost-system-dev help2man swig libeigen3-dev \
     	libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
@@ -85,13 +85,13 @@ install_ip_dependencies() {
     make -j 2
     echo
     sudo cp sta /usr/local/bin
-    #echo "|**********************************************************************************|"
-    #echo "|                                Installing netlistsvg                             |"
-    #echo "|**********************************************************************************|"
-    #echo
-    #cd
-    #sudo npm install -g nturley/netlistsvg
-    #echo
+    echo "|**********************************************************************************|"
+    echo "|                                Installing netlistsvg                             |"
+    echo "|**********************************************************************************|"
+    echo
+    cd
+    sudo npm install -g nturley/netlistsvg
+    echo
 }
 
 install_soc_dependencies() {
