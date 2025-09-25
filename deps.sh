7b3dfe23b91369818fb140321397f92d1e797f2e
EneaDim <eneadim95@gmail.com>
2025-09-25 09:23:22 +0200

    Trial

--- deps.sh
+++ deps.sh
@@ -13,78 +13,78 @@ install_ip_dependencies() {
       nodejs npm libreadline-dev gawk tcl-dev libffi-dev git autoconf cmake libelf-dev \
     	curl graphviz xdot pkg-config libboost-system-dev help2man swig libeigen3-dev \
     	libboost-python-dev libboost-filesystem-dev zlib1g-dev --assume-yes
-    echo "|**********************************************************************************|"
-    echo "|                               Installing iverilog                                |"
-    echo "|**********************************************************************************|"
-    echo
-    sudo apt-get install -y iverilog --assume-yes
-    echo "|**********************************************************************************|"
-    echo "|                                Installing gtkwave                                |"
-    echo "|**********************************************************************************|"
-    echo
-    cd
-    sudo apt install gtkwave --assume-yes
-    echo
-    echo
-    echo "|**********************************************************************************|"
-    echo "|                                Installing sv2v                                   |"
-    echo "|**********************************************************************************|"
-    echo
-    cd
-    curl -sSL https://get.haskellstack.org/ | sh
-    git clone https://github.com/zachjs/sv2v.git
-    cd sv2v
-    stack setup
-    stack install
-    echo
-    sudo cp $HOME/.local/bin/sv2v /usr/local/bin
-    echo
-    echo
-    echo "|**********************************************************************************|"
-    echo "|                                Installing verilator                              |"
-    echo "|**********************************************************************************|"
-    echo
-    cd
-    git clone https://github.com/verilator/verilator
-    cd verilator
-    git checkout v5.036 
-    autoconf
-    ./configure
-    make -j 2
-    sudo make install
-    echo
-    echo "|**********************************************************************************|"
-    echo "|            Cloning yosys and then will start the installation of yosys           |"
-    echo "|**********************************************************************************|"
-    echo
-    cd
-    git clone https://github.com/YosysHQ/yosys.git
-    cd yosys
-    git submodule update --init
-    make -j 2
-    sudo make install
-    cd
-    echo
-    echo "|**********************************************************************************|"
-    echo "|                              OpenSTA installation                                |"
-    echo "|**********************************************************************************|"
-    cd 
-    echo "| Download https://github.com/davidkebo/cudd/blob/main/cudd_versions/cudd-3.0.0.tar.gz |"
-    curl -L -o cudd-3.0.0.tar.gz https://github.com/davidkebo/cudd/raw/main/cudd_versions/cudd-3.0.0.tar.gz    
-    tar xvfz cudd-3.0.0.tar.gz
-    cd cudd-3.0.0
-    ./configure --prefix=/usr/local/bin
-    make -j 2
-    sudo make install
-    cd
-    git clone https://github.com/The-OpenROAD-Project/OpenSTA.git
-    cd OpenSTA
-    mkdir build
-    cd build
-    cmake -DCUDD_DIR=/usr/local/bin/lib ..
-    make -j 2
-    echo
-    sudo cp sta /usr/local/bin
+    #echo "|**********************************************************************************|"
+    #echo "|                               Installing iverilog                                |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #sudo apt-get install -y iverilog --assume-yes
+    #echo "|**********************************************************************************|"
+    #echo "|                                Installing gtkwave                                |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #cd
+    #sudo apt install gtkwave --assume-yes
+    #echo
+    #echo
+    #echo "|**********************************************************************************|"
+    #echo "|                                Installing sv2v                                   |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #cd
+    #curl -sSL https://get.haskellstack.org/ | sh
+    #git clone https://github.com/zachjs/sv2v.git
+    #cd sv2v
+    #stack setup
+    #stack install
+    #echo
+    #sudo cp $HOME/.local/bin/sv2v /usr/local/bin
+    #echo
+    #echo
+    #echo "|**********************************************************************************|"
+    #echo "|                                Installing verilator                              |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #cd
+    #git clone https://github.com/verilator/verilator
+    #cd verilator
+    #git checkout v5.036 
+    #autoconf
+    #./configure
+    #make -j 2
+    #sudo make install
+    #echo
+    #echo "|**********************************************************************************|"
+    #echo "|            Cloning yosys and then will start the installation of yosys           |"
+    #echo "|**********************************************************************************|"
+    #echo
+    #cd
+    #git clone https://github.com/YosysHQ/yosys.git
+    #cd yosys
+    #git submodule update --init
+    #make -j 2
+    #sudo make install
+    #cd
+    #echo
+    #echo "|**********************************************************************************|"
+    #echo "|                              OpenSTA installation                                |"
+    #echo "|**********************************************************************************|"
+    #cd 
+    #echo "| Download https://github.com/davidkebo/cudd/blob/main/cudd_versions/cudd-3.0.0.tar.gz |"
+    #curl -L -o cudd-3.0.0.tar.gz https://github.com/davidkebo/cudd/raw/main/cudd_versions/cudd-3.0.0.tar.gz    
+    #tar xvfz cudd-3.0.0.tar.gz
+    #cd cudd-3.0.0
+    #./configure --prefix=/usr/local/bin
+    #make -j 2
+    #sudo make install
+    #cd
+    #git clone https://github.com/The-OpenROAD-Project/OpenSTA.git
+    #cd OpenSTA
+    #mkdir build
+    #cd build
+    #cmake -DCUDD_DIR=/usr/local/bin/lib ..
+    #make -j 2
+    #echo
+    #sudo cp sta /usr/local/bin
     echo "|**********************************************************************************|"
     echo "|                                Installing netlistsvg                             |"
     echo "|**********************************************************************************|"
