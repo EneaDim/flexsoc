e9ab1e4451e97ed2d4bea7765b1bddac3e1fbbc1
EneaDim <eneadim95@gmail.com>
2025-10-17 15:39:37 +0200

    Minor fix

--- ips/soc/tb/soc_tb.sv
+++ ips/soc/tb/soc_tb.sv
@@ -116,10 +116,10 @@ module soc_tb;
   
   // Offsets (ADATTA ai tuoi registri reali)
   localparam logic [31:0] UART_CTRL_OFF   = 32'h0000_0010;  // CTRL: abilita tx/rx, nco ecc.
-  localparam logic [31:0] PWM_EN_OFF      = 32'h0000_000C;  // enable pwm block
-  localparam logic [31:0] PWM_CFG_OFF     = 32'h0000_0008;  // duty del canale 0
-  localparam logic [31:0] PWM_PHASE_OFF   = 32'h0000_0014;  // duty del canale 0
-  localparam logic [31:0] PWM_DUTY0_OFF   = 32'h0000_002C;  // duty del canale 0
+  localparam logic [31:0] PWM_EN_OFF      = 32'h0000_0008;  // enable pwm block
+  localparam logic [31:0] PWM_CFG_OFF     = 32'h0000_0004;  // duty del canale 0
+  localparam logic [31:0] PWM_PHASE_OFF   = 32'h0000_0010;  // duty del canale 0
+  localparam logic [31:0] PWM_DUTY0_OFF   = 32'h0000_0020;  // duty del canale 0
   localparam logic [31:0] SPI_CFG_OFF     = 32'h0000_0000;  // enable bit nello SPI host
   
   // Valori di esempio (ADATTA i bit-field alla tua mappa CTRL!)
