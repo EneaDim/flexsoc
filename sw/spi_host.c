2afe6dae0cec211cff3f8a2bb491041905114e98
EneaDim <eneadim95@gmail.com>
2025-10-28 15:16:59 +0100

    Take this driver in the main sw folder

---
+++ sw/spi_host.c
@@ -0,0 +1,31 @@
+#include "spi_host.h"
+
+#define DEV_WRITE(addr, val) (*((volatile uint32_t *)(addr)) = (val))
+#define DEV_READ(addr) (*((volatile uint32_t *)(addr)))
+
+int spi_host_init(spi_host_t spi_host) {
+  DEV_WRITE(spi_host + SPI_HOST_CTRL_REG_OFFSET, 0x1);
+  return 0;
+}
+
+int spi_host_in(spi_host_t spi_host) {
+  int res = -1;
+  res = DEV_READ(spi_host + SPI_HOST_RDATA_REG_OFFSET);
+  return res;
+}
+
+void spi_host_out(spi_host_t spi_host, char c) {
+  DEV_WRITE(spi_host + SPI_HOST_WDATA_REG_OFFSET, c);
+}
+
+int spi_host_putchar(int c) {
+  spi_host_out((spi_host_t) SPI_HOST_BASE, c);
+  return c;
+}
+
+int spi_host_puts(const char* str) {
+  while (*str) {
+    spi_host_putchar(*str++);
+  }
+  return 0;
+}
